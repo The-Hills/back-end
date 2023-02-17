fd108ea4b44a93c5dd7477a5c1e1dfdd9c1e41bf
HuyND11 <huy.nguyen23@student.passerellesnumeriques.org>
2023-02-17 15:48:37 +0700

    S3 bucket config

--- src/services/s3Client.service.ts
+++ src/services/s3Client.service.ts
@@ -1,5 +1,33 @@
-import { S3Client } from "@aws-sdk/client-s3";
+import * as AWS from "aws-sdk";
+import * as fs from "fs";
+import * as dotenv from "dotenv";
 
-const REGION = "REGION"; //e.g. "us-east-1"
+dotenv.config();
 
-const s3Client = new S3Client({ region: REGION });
+AWS.config.update({
+  region: process.env.REGION,
+  credentials: {
+    accessKeyId: process.env.ACCESSKEYID,
+    secretAccessKey: process.env.SECRETACCESSKEY,
+  },
+});
+
+const s3 = new AWS.S3({
+  credentials: {
+    accessKeyId: process.env.ACCESSKEYID,
+    secretAccessKey: process.env.SECRETACCESSKEY,
+  },
+});
+
+const uploadImage = async (file) => {
+  const fileStream = fs.createReadStream(file.path);
+  const params = {
+    Bucket: process.env.BUCKET,
+    Body: fileStream,
+    Key: file.filename,
+  };
+  const uploadImage = await s3.upload(params).promise();
+  return uploadImage.Location;
+};
+
+export default uploadImage;
