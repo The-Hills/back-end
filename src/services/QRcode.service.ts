3e80cb123317fa054a0e7b0f1b737972dbf6fea9
HuyND11 <huy.nguyen23@student.passerellesnumeriques.org>
2023-03-10 14:57:06 +0700

    Remove function generate QRcode

--- src/services/QRcode.service.ts
+++
@@ -1,9 +0,0 @@
-import * as QRCode from "qrcode";
-
-const generateQR = async (id: string) => {
-  console.log(id)
-  const QR = await QRCode.toDataURL(id);
-  return QR
-};
-
-export default generateQR;
